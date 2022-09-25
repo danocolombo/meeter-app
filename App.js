ebc7a692f0d40ae7cd6f27e66be6cea9524254b2
danocolombo@google.com <GR0mans1212!>
2022-09-25 18:25:49 -0400

    up and running, lets make it work now

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+Amplify.configure({
+    ...awsconfig,
+    Analytics: {
+        disabled: true,
+    },
+});
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const MAPI = process.env.MEETER_API;
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+
+            <StatusBar style='auto' />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
